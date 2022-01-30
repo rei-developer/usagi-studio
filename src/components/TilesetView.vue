efabf5f348e9822b7cda4966ea226839df07d457
INYEOB <deran2016@gmail.com>
2022-01-30 18:12:32 +0900

    Draw a rectangle of selected tiles

--- src/components/TilesetView.vue
+++ src/components/TilesetView.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-// const TILESIZE = 32;
+const TILESIZE = 32;
 
 export default {
     name: 'TilesetView',
@@ -24,10 +24,33 @@ export default {
         width: 0,
         height: 0,
         tileset: null,
+        tileSelectStart: null,
+        selection: [],
     }),
 
     created() {
         this.init();
+    },
+
+    mounted() {
+        this.$el.querySelector('#tilesetCanvas').addEventListener('pointerdown', (e) => {
+            this.tileSelectStart = this.getSelectedTile(e)[0];
+        });
+        this.$el.querySelector('#tilesetCanvas').addEventListener('pointermove', (e) => {
+            if (this.tileSelectStart) {
+                this.selection = this.getSelectedTile(e);
+                const width = this.selection[this.selection.length - 1].x - this.selection[0].x + 1
+                const height = this.selection[this.selection.length - 1].y - this.selection[0].y + 1
+                this.drawSelectedTile(this.selection[0].x, this.selection[0].y, width, height);
+            }
+        });
+        this.$el.querySelector('#tilesetCanvas').addEventListener('pointerup', (e) => {
+            this.selection = this.getSelectedTile(e);
+            this.tileSelectStart = null;
+            const width = this.selection[this.selection.length - 1].x - this.selection[0].x + 1
+            const height = this.selection[this.selection.length - 1].y - this.selection[0].y + 1
+            this.drawSelectedTile(this.selection[0].x, this.selection[0].y, width, height);
+        });
     },
 
     methods: {
@@ -36,7 +59,6 @@ export default {
             this.tileset.src = require(`@/assets/tilesets/${this.tilesetName}.png`);
             this.width = this.tileset.width;
             this.height = this.tileset.height;
-
             this.tileset.onload = () => {
                 this.draw();
             };
@@ -44,7 +66,6 @@ export default {
 
         draw() {
             const ctx = this.getContext();
-            console.log(this.width, this.height);
             ctx.clearRect(0, 0, this.width, this.height);
             ctx.drawImage(this.tileset, 0, 0, this.width, this.height);
         },
@@ -52,7 +73,38 @@ export default {
         getContext() {
             const canvas = this.$el.querySelector('#tilesetCanvas');
             return canvas.getContext('2d');
-        }
+        },
+
+        getSelectedTile(event) {
+            const offset = 384;
+            const { x, y } = event.target.getBoundingClientRect();
+            const tx = Math.floor(Math.max(event.clientX - x, 0) / TILESIZE);
+            const ty = Math.floor(Math.max(event.clientY - y, 0) / TILESIZE);
+
+            const newSelection = [];
+            if (this.tileSelectStart) {
+                for (let ix = this.tileSelectStart.x; ix < tx + 1; ix++) {
+                    for (let iy = this.tileSelectStart.y; iy < ty + 1; iy++) {
+                        const data = iy * 8 + ix + offset;
+                        newSelection.push({...data, x: ix, y: iy});
+                    }
+                }
+            }
+            if (newSelection.length > 0) return newSelection;
+
+            const data = ty * 8 + tx + offset;
+            return [{...data, x: tx, y: ty}]
+        },
+
+        drawSelectedTile(x, y, width, height) {
+            const ctx = this.getContext();
+            this.draw();
+            ctx.beginPath();
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+            ctx.rect(x * TILESIZE, y * TILESIZE, width * TILESIZE, height * TILESIZE);
+            ctx.stroke();
+        },
     },
 }
 </script>
