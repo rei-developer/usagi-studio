ab3a9f720f7c6846ac354328e2355022622efe9d
nakochan <46469738+nakochan@users.noreply.github.com>
2022-02-02 20:36:51 +0300

    좌표 로직 수정

--- src/components/MapTools.vue
+++ src/components/MapTools.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
     <ui-button size="lg" />
-    <span>{{
-      pointer[0] !== undefined
-        ? `마우스 좌표: (${pointer[0]}, ${pointer[1]})`
-        : ""
-    }}</span>
+    <span v-if="isInside">{{ `마우스 좌표: (${x}, ${y})` }}</span>
     <div>{{ `너비: ${width}, 높이: ${height}` }}</div>
   </div>
 </template>
@@ -28,6 +24,17 @@ export default {
   watch: {
     ACTIVE_TOOL() {
       this.$emit("toolSelected", this.ACTIVE_TOOL);
+    },
+  },
+  computed: {
+    x() {
+      return this.pointer[0];
+    },
+    y() {
+      return this.pointer[1];
+    },
+    isInside() {
+      return this.x !== undefined && this.y !== undefined;
     },
   },
   methods: {
